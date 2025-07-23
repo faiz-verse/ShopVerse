46ecd74541e9db87e8125d54f0851d647f3f03bf
faiz-verse <faizzzverseee@gmail.com>
2025-07-23 10:45:02 +0530

    Working . . .

--- shopverse-frontend/src/components/Nav.tsx
+++ shopverse-frontend/src/components/Nav.tsx
@@ -4,11 +4,14 @@ import './Nav.css'
 import { Link } from 'react-router'
 
 import { type IconBaseProps } from 'react-icons'
-import { BsSearchHeart } from 'react-icons/bs'
+import { BsSearchHeart, BsPersonHeart, BsBoxArrowInRight, BsCart2 } from 'react-icons/bs'
 
 const Nav = () => {
 
     const BsSearch = BsSearchHeart as React.ComponentType<IconBaseProps>;
+    const BsPerson = BsPersonHeart as React.ComponentType<IconBaseProps>;
+    const BsLogin = BsBoxArrowInRight as React.ComponentType<IconBaseProps>;
+    const BsCart = BsCart2 as React.ComponentType<IconBaseProps>;
 
     return (
         <div id='Nav'>
@@ -16,16 +19,14 @@ const Nav = () => {
                 <img src="shopping-cart.png" alt="logo" />
                 <span>ShopVerse</span>
             </div>
+            <div id='nav-search'>
+                <input type="text" placeholder='Search Product' />
+                <BsSearch id='search-icon' size={24} color='rgb(150, 150, 150)' />
+            </div>
             <ul id='nav-pages'>
-                <div id='nav-search'>
-                    <input type="text" placeholder='Search Product' />
-                    <BsSearch id='search-icon' size={24} color='gray' />
-                </div>
-                <Link to="/cart"><li>Cart</li></Link>
-                <Link to="/order-history"><li>Order History</li></Link>
-                <Link to="/login"><li>Log in</li></Link>
-                <Link to="/profile"><li>Profile</li></Link>
-                <Link to="/admin-dashboard"><li>Admin Dashboard</li></Link>
+                <Link to="/cart"><li>Cart <BsCart id='search-icon' size={24} color='black' /></li></Link>
+                <Link to="/login"><li>Log in <BsLogin id='search-icon' size={24} color='black' /></li></Link>
+                <Link to="/profile"><li>Profile <BsPerson id='search-icon' size={24} color='black' /></li></Link>
             </ul>
         </div>
     )
