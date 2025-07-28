6bd7304859c255911ddd7b2bcbfd46ad466e3789
faiz-verse <faizzzverseee@gmail.com>
2025-07-28 18:56:41 +0530

    Done with the Nav and worked with the banner

--- shopverse-frontend/src/components/Nav.tsx
+++ shopverse-frontend/src/components/Nav.tsx
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Nav.css'
 
 import { Link } from 'react-router'
 
 import { type IconBaseProps } from 'react-icons'
-import { BsSearchHeart, BsPersonHeart, BsBoxArrowInRight, BsCart2 } from 'react-icons/bs'
+import { BsSearchHeart, BsPersonHeart, BsBoxArrowInRight, BsCart2, BsList, BsX } from 'react-icons/bs'
 
 const Nav = () => {
 
-    const BsSearch = BsSearchHeart as React.ComponentType<IconBaseProps>;
-    const BsPerson = BsPersonHeart as React.ComponentType<IconBaseProps>;
-    const BsLogin = BsBoxArrowInRight as React.ComponentType<IconBaseProps>;
-    const BsCart = BsCart2 as React.ComponentType<IconBaseProps>;
+    const BsSearch = BsSearchHeart as React.ComponentType<IconBaseProps>
+    const BsPerson = BsPersonHeart as React.ComponentType<IconBaseProps>
+    const BsLogin = BsBoxArrowInRight as React.ComponentType<IconBaseProps>
+    const BsCart = BsCart2 as React.ComponentType<IconBaseProps>
+    const BsMenu = BsList as React.ComponentType<IconBaseProps>
+    const BsClose = BsX as React.ComponentType<IconBaseProps>
+
+    const [isExpanded, setIsExpanded] = useState(false);
 
     return (
         <div id='Nav'>
@@ -30,6 +34,19 @@ const Nav = () => {
                 <Link to="/login"><li>Log in <BsLogin id='search-icon' size={24} color='black' /></li></Link>
                 <Link to="/profile"><li>Profile <BsPerson id='search-icon' size={24} color='black' /></li></Link>
             </ul>
+
+            {/* Menu */}
+            <div id='nav-expand'>
+                <BsMenu id='nav-ham' size={24} color='black' onClick={() => setIsExpanded(!isExpanded)} />
+                <div id='nav-options' style={{ right: isExpanded ? '0px' : '-100vw' }}>
+                    <BsClose size={24} color='black' onClick={() => setIsExpanded(!isExpanded)} />
+                    <ul>
+                        <Link to="/cart"><li onClick={() => setIsExpanded(!isExpanded)}>Cart <BsCart id='search-icon' size={24} color='black' /></li></Link>
+                        <Link to="/login"><li onClick={() => setIsExpanded(!isExpanded)}>Log in <BsLogin id='search-icon' size={24} color='black' /></li></Link>
+                        <Link to="/profile"><li onClick={() => setIsExpanded(!isExpanded)}>Profile <BsPerson id='search-icon' size={24} color='black' /></li></Link>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
--- shopverse-frontend/src/pages/Home.tsx
+++ shopverse-frontend/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './Home.css'
 // import { Link } from 'react-router'
 
@@ -34,27 +34,80 @@ const Home = () => {
     productTypes.forEach((c) => console.log(c.type, " ", c.repetition))
 
 
+    // for banner
+    const [currentBanner, setCurrentBanner] = React.useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentBanner(prev => (prev === banner.length ? 0 : prev + 1))
+        }, 5000)
+        return () => clearInterval(interval)
+    }, [banner.length])
+
+    // for the last clone banner
+    useEffect(() => {
+        if (currentBanner === banner.length) {
+            // wait until transition ends
+            const timeout = setTimeout(() => {
+                setCurrentBanner(0); // jump back to start
+            }, 700); // match your transition duration
+            return () => clearTimeout(timeout);
+        }
+    }, [currentBanner, banner.length]);
+
+    // handling prev and next
+    // const handlePrev = () => {
+    //     setCurrentBanner((prev) => (prev === 0 ? banner.length : prev - 1));
+    // };
+    const handleNext = () => {
+        setCurrentBanner((prev) => (prev === banner.length ? 0 : prev + 1));
+    };
+
     return (
         <div id='Home'>
-            <div id='banner'>
-                {banner.map((b, index) => {
-                    return (
-                        <div key={index} className="banner-content" style={{ ["--position" as any]: `${index}`, fontStyle: b.fontStyle, color: b.fontColor }}>
-                            <img src={b.imageLink} alt="banner image" />
-                            <div className='banner-details'>
-                                <div className='banner-title' style={{ fontSize: b.titleFontSize, fontFamily: b.titleFontFamily }}>{b.title}</div>
-                                <div className='banner-info' style={{ fontSize: b.descriptionFontSize, fontFamily: b.descriptionFontFamily }}>{b.description}</div>
-                                <button className='redirect-button' style={{ background: b.redirectButtonColor }}>{b.redirectButtonText}</button>
+            <div id='banner-wrapper'>
+                <div id='banner'>
+                    <div
+                        style={{
+                            display: 'flex',
+                            transition: 'transform 0.7s cubic-bezier(0.77,0,0.18,1)',
+                            transform: `translateX(-${currentBanner * 100}%)`,
+                            transitionDuration: currentBanner === 0 ? "0s" : "0.7s"
+                        }}
+                    >
+                        {banner.map((b, index) => (
+                            <div key={index} className="banner-content" style={{ flexShrink: 0, fontStyle: b.fontStyle, color: b.fontColor }}>
+                                <img src={b.imageLink} alt="banner image" loading='lazy' />
+                                <div className='banner-details' style={{
+                                    left: b.position === "left" ? "50px" : "none",
+                                    right: b.position === "right" ? "50px" : "none"
+                                }}>
+                                    <div className='banner-title' style={{ fontSize: b.titleFontSize, fontFamily: b.titleFontFamily }}>{b.title}</div>
+                                    <div className='banner-info' style={{ fontSize: b.descriptionFontSize, fontFamily: b.descriptionFontFamily }}>{b.description}</div>
+                                    <button className='redirect-button' style={{ background: b.redirectButtonColor, color: b.redirectButtonTextColor }}>{b.redirectButtonText}</button>
+                                </div>
+                            </div>
+                        ))}
+                        {/* last banner = first banner */}
+                        <div className="banner-content" style={{ flexShrink: 0, fontStyle: banner[0].fontStyle, color: banner[0].fontColor }}>
+                            <img src={banner[0].imageLink} alt="banner image" loading='lazy' />
+                            <div className='banner-details' style={{
+                                left: banner[0].position === "left" ? "50px" : "none",
+                                right: banner[0].position === "right" ? "50px" : "none"
+                            }}>
+                                <div className='banner-title' style={{ fontSize: banner[0].titleFontSize, fontFamily: banner[0].titleFontFamily }}>{banner[0].title}</div>
+                                <div className='banner-info' style={{ fontSize: banner[0].descriptionFontSize, fontFamily: banner[0].descriptionFontFamily }}>{banner[0].description}</div>
+                                <button className='redirect-button' style={{ background: banner[0].redirectButtonColor, color: banner[0].redirectButtonTextColor }}>{banner[0].redirectButtonText}</button>
                             </div>
                         </div>
-                    );
-                })}
 
-                <button className='banner-button' id='banner-before'>
-                    <BsArLeft size={24} color='black' />
-                </button>
-                <button className='banner-button' id='banner-after'>
-                    <BsArRight size={24} color='black' />
+                    </div>
+                </div>
+                {/* <button className='banner-button' id='banner-before' onClick={handlePrev}>
+                    <BsArLeft size={24} color='purple' />
+                </button> */}
+                <button className='banner-button' id='banner-after' onClick={handleNext}>
+                    <BsArRight size={24} color='purple' />
                 </button>
             </div>
 
