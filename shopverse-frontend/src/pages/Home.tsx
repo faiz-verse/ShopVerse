dab91c210c29fa19746c3b37d4a7e25b1159d088
faiz-verse <faizzzverseee@gmail.com>
2025-09-11 12:08:29 +0530

    commented unused imports

--- shopverse-frontend/src/pages/Home.tsx
+++ shopverse-frontend/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+// import React, { useEffect, useRef, useState } from 'react'
 import './Home.css'
 // import { Link } from 'react-router'
 
